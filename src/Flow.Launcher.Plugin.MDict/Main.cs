c7da8e56b95b6155a1847a33c0dbb84c4e464066
Jack251970 <1160210343@qq.com>
2025-07-05 16:02:41 +0800

    Add folder

---
+++ src/Flow.Launcher.Plugin.MDict/Main.cs
@@ -0,0 +1,213 @@
+using MDict.Csharp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Windows.Controls;
+
+namespace Flow.Launcher.Plugin.MDict;
+
+public class Main : IPlugin, IPluginI18n, ISettingProvider, IDisposable
+{
+    internal static PluginInitContext Context { get; private set; } = null!;
+
+    internal static Settings Settings { get; private set; } = null!;
+
+    internal const string VirtualHost = "appassets";
+
+    internal static MdxDict? Dict { get; private set; }
+    internal static string? DictDirectory { get; private set; }
+
+    internal static Dictionary<string, string> WebView2PathMapping { get; } = new();
+
+    private readonly static string ClassName = nameof(Main);
+
+    #region IPlugin Interface
+
+    public List<Result> Query(Query query)
+    {
+        if (Dict == null)
+        {
+            return new List<Result>
+            {
+                new()
+                {
+                    Title = Localize.flowlauncher_plugin_mdict_plugin_please_select_dictionary(),
+                    IcoPath = Context.CurrentPluginMetadata.IcoPath,
+                    Action = _ =>
+                    {
+                        Context.API.OpenSettingDialog();
+                        return true;
+                    }
+                }
+            };
+        }
+
+        var querySearch = query.Search;
+        if (string.IsNullOrEmpty(querySearch))
+        {
+            return new List<Result>
+            {
+                new()
+                {
+                    Title = Localize.flowlauncher_plugin_mdict_plugin_please_type_query(),
+                    IcoPath = Context.CurrentPluginMetadata.IcoPath
+                }
+            };
+        }
+        else
+        {
+            var results = new List<Result>();
+            var score = Settings.FuzzySize;
+            foreach (var word in Dict.FuzzySearch(querySearch, Settings.FuzzySize, (uint)Settings.Distance))
+            {
+                results.Add(new Result
+                {
+                    Title = word.KeyText,
+                    IcoPath = Context.CurrentPluginMetadata.IcoPath,
+                    Action = _ =>
+                    {
+                        // TODO
+                        return false;
+                    },
+                    Score = score,
+                    AddSelectedCount = false,
+                    PreviewPanel = new Lazy<UserControl>(() => new PreviewPanel(word))
+                });
+                score--;
+            }
+            return results;
+        }
+    }
+
+    public void Init(PluginInitContext context)
+    {
+        Context = context;
+
+        // Init settings
+        Settings = context.API.LoadSettingJsonStorage<Settings>();
+        Settings.PropertyChanged += Settings_PropertyChanged;
+        Context.API.LogDebug(ClassName, $"Init: {Settings}");
+
+        // Load dictionary
+        LoadDictionary(Settings.DictPath);
+    }
+
+    private void Settings_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        switch (e.PropertyName)
+        {
+            case nameof(Settings.DictPath):
+                if (File.Exists(Settings.DictPath))
+                {
+                    LoadDictionary(Settings.DictPath);
+                }
+                break;
+        }
+    }
+
+    #endregion
+
+    #region Dictionary Management
+
+    private static void LoadDictionary(string path)
+    {
+        // Check path exists
+        if (string.IsNullOrEmpty(path) || !File.Exists(path)) return;
+
+        // Close the previous dictionary if it exists
+        Dict?.Close();
+
+        // Load the new dictionary
+        Dict = new MdxDict(path);
+
+        // Load the web view paths
+        DictDirectory = Path.GetDirectoryName(path);
+        if (!string.IsNullOrEmpty(DictDirectory))
+        {
+            WebView2PathMapping.Clear();
+            foreach (var file in Directory.EnumerateFiles(DictDirectory))
+            {
+                var relatedPath = Path.GetRelativePath(DictDirectory, file);
+                if (!string.IsNullOrEmpty(relatedPath))
+                {
+                    // If related path contains backslashes or forward slashes, we need to add two versions of replacement rules
+                    if (relatedPath.Contains('\\'))
+                    {
+                        var unixRelatedPath = relatedPath.Replace('\\', '/');
+                        WebView2PathMapping[relatedPath] = $"https://{VirtualHost}/{unixRelatedPath}";
+                        WebView2PathMapping[unixRelatedPath] = $"https://{VirtualHost}/{unixRelatedPath}";
+                    }
+                    else if (relatedPath.Contains('/'))
+                    {
+                        var winRelatedPath = relatedPath.Replace('/', '\\');
+                        WebView2PathMapping[relatedPath] = $"https://{VirtualHost}/{relatedPath}";
+                        WebView2PathMapping[winRelatedPath] = $"https://{VirtualHost}/{relatedPath}";
+                    }
+                    // Else just add the related path as it is
+                    else
+                    {
+                        WebView2PathMapping[relatedPath] = $"https://{VirtualHost}/{relatedPath}";
+                    }
+                }
+            }
+        }
+    }
+
+    #endregion
+
+    #region IPluginI18n Interface
+
+    public string GetTranslatedPluginTitle()
+    {
+        return Localize.flowlauncher_plugin_mdict_plugin_name();
+    }
+
+    public string GetTranslatedPluginDescription()
+    {
+        return Localize.flowlauncher_plugin_mdict_plugin_description();
+    }
+
+    public void OnCultureInfoChanged(CultureInfo cultureInfo)
+    {
+
+    }
+
+    #endregion
+
+    #region ISettingProvider Interface
+
+    public Control CreateSettingPanel()
+    {
+        Context.API.LogDebug(ClassName, $"Settings Panel: {Settings}");
+        return new SettingsPanel(Settings);
+    }
+
+    #endregion
+
+    #region IDisposable
+
+    private bool _disposed;
+
+    public void Dispose()
+    {
+        if (_disposed)
+        {
+            Dict?.Close();
+            return;
+        }
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _disposed = true;
+        }
+    }
+
+    #endregion
+}
---
+++ src/Flow.Launcher.Plugin.MDict/PreviewPanel.xaml.cs
@@ -0,0 +1,64 @@
+﻿using MDict.Csharp.Models;
+using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Flow.Launcher.Plugin.MDict;
+
+public partial class PreviewPanel : UserControl
+{
+    public string KeyText => _word.KeyText;
+
+    private readonly FuzzyWord _word;
+
+    public PreviewPanel(FuzzyWord word)
+    {
+        _word = word ?? throw new ArgumentNullException(nameof(word));
+        InitializeComponent();
+        // TODO: Use true dark mode if available
+        // TODO: Update ResultWebView2 when the system theme changes
+        UpdateResultWebView2(true);
+    }
+
+    private async void UpdateResultWebView2(bool isDarkTheme)
+    {
+        if (Main.Dict is null || string.IsNullOrEmpty(Main.DictDirectory) || _word is null || ResultWebView2 is null) return;
+
+        var (_, Definition) = Main.Dict.Fetch(_word);
+        if (Definition is null) return;
+
+        var newDefinition = new StringBuilder(Definition);
+
+        // Replace the light and dark theme CSS based on the system theme
+        if (!string.IsNullOrEmpty(Main.Settings.LightThemeCss) && !string.IsNullOrEmpty(Main.Settings.DarkThemeCss))
+        {
+            if (isDarkTheme)
+            {
+                newDefinition.Replace(Main.Settings.LightThemeCss, Main.Settings.DarkThemeCss);
+            }
+            else
+            {
+                newDefinition.Replace(Main.Settings.DarkThemeCss, Main.Settings.LightThemeCss);
+            }
+        }
+
+        // Replace relative paths with virtual host urls
+        foreach (var kvp in Main.WebView2PathMapping)
+        {
+            newDefinition.Replace(kvp.Key, kvp.Value);
+        }
+
+        ResultWebView2.Visibility = Visibility.Visible;
+
+        await ResultWebView2.EnsureCoreWebView2Async();
+
+        ResultWebView2.CoreWebView2.SetVirtualHostNameToFolderMapping(
+            Main.VirtualHost,
+            Main.DictDirectory,
+            Microsoft.Web.WebView2.Core.CoreWebView2HostResourceAccessKind.Allow
+        );
+
+        ResultWebView2.NavigateToString(newDefinition.ToString());
+    }
+}
