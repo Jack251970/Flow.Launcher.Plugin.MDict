301eed6e0f12b0cc64a9f0543c7b545e2d77bb8b
Jack251970 <1160210343@qq.com>
2025-07-05 15:27:27 +0800

    Fix build script

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 Set-Location -Path $PSScriptRoot
 
 # Debug: Confirm the directory (optional)
-Write-Host "Build Clipboard+ in: $(Get-Location)"
+Write-Host "Build MDict in: $(Get-Location)"
 
 # Run the build command (path is now relative to the script's location)
 dotnet run --project build/Build.csproj
